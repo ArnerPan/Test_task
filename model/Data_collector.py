80ba65d837dcceecb13f91c6a68bd2adc2f6970f
Arner Pan <sergalbox@gmail.com>
2022-07-03 23:24:53 +0300

    optimized local .venv

--- model/Data_collector.py
+++ model/Data_collector.py
@@ -2,6 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import chromedriver_binary
+from selenium.webdriver.common.by import By
 
 
 def get_links_from(url: str = "https://www.mos.ru/", avoid_duplicate: bool = False):
@@ -18,7 +19,7 @@ def get_links_from(url: str = "https://www.mos.ru/", avoid_duplicate: bool = Fal
         scroll = scroll + scroll_step
         driver.execute_script(f"window.scrollTo(0, {scroll})")
         time.sleep(0.01)
-    founded_links = driver.find_elements_by_css_selector(":link")
+    founded_links = driver.find_elements(By.CSS_SELECTOR, ":link")
     if avoid_duplicate is True:
         links = []
         for link_element in founded_links:
